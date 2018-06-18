8d483d5ef10fd82946aa049c36d57400b1cb401e
John VanEnk <jvanenk@fastly.com>
2018-06-18 14:56:01 -0700

    Add test script.

---
+++ test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+glist_a = []
+glist_b = []
+
+def call_branch_a(a,b):
+    pass
+
+def call_branch_b(a,b):
+    pass
+
+def call_branch_c(a,b):
+    pass
+
+call_branch_a(glist_a,glist_b)
+call_branch_b(glist_a,glist_b)
+call_branch_c(glist_a,glist_b)
+
+exit(sum(glist_a) + sum(glist_b))
